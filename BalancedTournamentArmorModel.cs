e7c0335bd41d5e7ccba65636401e33e790a41aa0
OrderWOPower <82205477+OrderWOPower@users.noreply.github.com>
2025-12-13 14:56:38 +0800

    Removed Banner Kings as a dependency

--- BalancedTournamentArmorModel.cs
+++ BalancedTournamentArmorModel.cs
@@ -23,7 +23,7 @@ namespace BalancedTournamentArmor
             if (Mission.Current.Mode == MissionMode.Tournament)
             {
                 // Get troop armors of the current settlement's culture.
-                equipment = CharacterObject.FindAll(character => character.Culture == Settlement.CurrentSettlement.Culture && character.Tier == BalancedTournamentArmorSettings.Instance.TroopTier && character.IsSoldier && !character.HiddenInEncyclopedia && !character.IsFemale && !character.StringId.Contains("tutorial") && !character.StringId.Contains("conspiracy") && !character.StringId.Contains("root") && !character.StringId.Contains("canticles")).GetRandomElementInefficiently()?.RandomBattleEquipment;
+                equipment = CharacterObject.FindAll(character => character.Culture == Settlement.CurrentSettlement.Culture && character.Tier == BalancedTournamentArmorSettings.Instance.TroopTier && character.IsSoldier && !character.HiddenInEncyclopedia && !character.IsFemale && !character.StringId.Contains("tutorial") && !character.StringId.Contains("conspiracy") && !character.StringId.Contains("root")).GetRandomElementInefficiently()?.RandomBattleEquipment;
 
                 if (equipment == null)
                 {
